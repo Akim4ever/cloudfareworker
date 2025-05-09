369e8ffc343f0b7f3d35fea3724b24533ee7a444
Akim4ever <cyn@solver-sys.online>
2025-05-09 11:17:12 +0300

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,3 +1,11 @@
-export async function onRequest(context) {
-  return new Response("Test successful", { status: 200 });
-}
+addEventListener('fetch', event => {
+      const originalRequest = event.request;
+
+        const modifiedRequest = new Request(originalRequest, {
+            cf: {
+                  resolveOverride: 'srv-cdn.solver.net.co'
+                      }
+                        });
+
+                          event.respondWith(fetch(modifiedRequest));
+                          });
