5ae0870b10d44b7c6aa7b5d836a17fdae48a78b0
Akim4ever <cyn@solver-sys.online>
2025-05-09 11:23:13 +0300

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,11 +1,17 @@
-addEventListener('fetch', event => {
-      const originalRequest = event.request;
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const proxyUrl = new URL(url);
+  proxyUrl.hostname = 'srv-cdn.solver.net.co';
 
-        const modifiedRequest = new Request(originalRequest, {
-            cf: {
-                  resolveOverride: 'srv-cdn.solver.net.co'
-                      }
-                        });
+  const headers = new Headers(context.request.headers);
+  headers.set('Host', 'srv-cdn.solver.net.co'); // попытка установить явно
 
-                          event.respondWith(fetch(modifiedRequest));
-                          });
+  const modifiedRequest = new Request(proxyUrl.toString(), {
+    method: context.request.method,
+    headers,
+    body: context.request.body,
+    redirect: 'follow',
+  });
+
+  return fetch(modifiedRequest);
+}
